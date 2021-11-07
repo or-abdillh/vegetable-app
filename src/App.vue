ac250a0281086d81fbf0609ffff0e91c9b75f69f
or-abdillh <hans.abdillh05@gmail.com>
2021-11-07 11:18:41 +0800

    Create navbar

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 	<Header></Header>
 	<SearchBar></SearchBar>
 	<CardWrapper></CardWrapper>
+	<Navbar></Navbar>
 </template>
 
 <script setup>
    
-   import Header from './components/Header.vue'
-   import SearchBar from './components/SearchBar.vue'
-   import CardWrapper from './components/CardWrapper.vue'
-   
+	import Header from './components/Header.vue'
+	import SearchBar from './components/SearchBar.vue'
+	import CardWrapper from './components/CardWrapper.vue'
+	import Navbar from './components/Navbar.vue'
+      
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+	<section class="fixed shadow-cstm flex justify-between bottom-0 left-0 right-0 bg-white px-3 py-2">
+		<template v-for="list in lists" :key="list">
+			<div  :class="list.name === 'Search' ? 'text-green-500' : 'text-gray-600'">
+				<i :class="list.icon" class="w-full text-base text-center"></i>
+				<p class="text-xxs">{{ list.name }}</p>
+			</div>
+		</template>
+	</section>
+</template>
+
+<style scoped>
+
+	.text-xxs {
+		font-size: .6rem
+	}
+
+	.shadow-cstm {
+		box-shadow: 4px -1px 12px #BBBBBB;
+	}
+
+</style>
+
+<script setup>
+
+	const lists = [
+		{
+			name: 'Home',
+			icon: 'fa fa-home'
+		},
+		{
+			name: 'Search',
+			icon: 'fa fa-search'
+		},
+		{
+			name: 'Cart',
+			icon: 'fas fa-shopping-cart'
+		},
+		{
+			name: 'Favorite',
+			icon: 'far fa-heart'
+		},
+		{
+			name: 'Account',
+			icon: 'fa fa-user'
+		}
+	]
+
+</script>
